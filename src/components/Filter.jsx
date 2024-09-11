8a19650a135117a6249688a9fe8f2ddf3ab6f8c4
Himanshu Joshi <hiimanshuhh16@gmail.com>
2024-09-12 00:27:18 +0530

    updating

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -3,11 +3,14 @@ import React from 'react'
 const Filter = () => {
   let prods =[
     {id:1, name:"Product1", category: "house"},
-    {id:1, name:"Product2", category: "house"},
-    {id:1, name:"Product3", category: "outdoor"},
-    {id:1, name:"Product4", category: "outdoor"},
-    {id:1, name:"Product5", category: "indoor"},
-    {id:1, name:"Product6", category: "indoor"},
+    {id:2, name:"Product2", category: "house"},
+    {id:3, name:"Product3", category: "house"},
+    {id:4, name:"Product4", category: "outdoor"},
+    {id:5, name:"Product5", category: "outdoor"},
+    {id:6, name:"Product6", category: "outdoor"},
+    {id:7, name:"Product7", category: "indoor"},
+    {id:8, name:"Product8", category: "indoor"},
+    {id:9, name:"Product9", category: "indoor"},
   ]
   const filData = prods.filter((data)=>data.category == "indoor")
   console.log(filData);
