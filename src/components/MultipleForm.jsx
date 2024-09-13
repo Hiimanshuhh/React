5c5508c1985607c18c6d758c82cfa37771be14e3
Himanshu Joshi <hiimanshuhh16@gmail.com>
2024-09-13 11:03:16 +0530

    updating

--- src/components/MultipleForm.jsx
+++ src/components/MultipleForm.jsx
@@ -11,16 +11,23 @@ const MultipleForm = () => {
   })
   const formHandler =(e)=>{
     const{name,value} = e.target;
+    setFormData({...formData,[name]:value})
+  }
+  const submitHandler=(e)=>{
+    e.preventDefault();
+    alert("Your Form Has Been Submitted");
+    console.log(formData);
+    setFormData(" ");
   }
   return (
     <>
     <div>
-      <form>
-        <div><input type="text"  value={formData.name} name='name'/></div>
-        <div><input type="email" value={formData.email} name='email'/></div>
-        <div><input type="password" value={formData.password} name='password'/></div>
-        <div><input type="number" value={formData.age} name='age'/></div>
-        <div><input type="number" value={formData.phone} name='phone'/></div>
+      <form onSubmit={submitHandler}>
+        <div><input type="text"  value={formData.name} name='name' onChange={formHandler}/></div>
+        <div><input type="email" value={formData.email} name='email' onChange={formHandler}/></div>
+        <div><input type="password" value={formData.password} name='password' onChange={formHandler}/></div>
+        <div><input type="number" value={formData.age} name='age' onChange={formHandler}/></div>
+        <div><input type="number" value={formData.phone} name='phone' onChange={formHandler}/></div>
         <div><button>Submit</button></div>
       </form>
     </div>
