96d58da12d54ff45fbe4d1f21fa9a573ac680444
Himanshu Joshi <hiimanshuhh16@gmail.com>
2024-09-14 12:21:55 +0530

    updating

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 
 const About = () => {
+  const arr = [
+    {id: "Mern", course_name: "Mern_Stack", price: "50000", duration: "5 months"},
+    {id: "Java", course_name: "Java", price: "40000", duration: "5 months"},
+    {id: "Python", course_name: "Python", price: "30000", duration: "5 months"},
+    {id: "Cpp", course_name: "Cpp", price: "20000", duration: "5 months"}
+  ]
   return (
     <>
     <div>
-      <h1>Hi You are on About page :? </h1>
+      <ul>
+        {arr.map((data)=><div key={data.id}>
+          <li>{data.course_name}</li>
+        </div>)}
+      </ul>
     </div>
     </>
   )
